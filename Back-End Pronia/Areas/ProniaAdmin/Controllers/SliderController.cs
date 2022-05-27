59c61bb2a31c25ef17b74e4ce85b90d10e3b1cd4
Ali <tu8cvbxnx@code.edu.az>
2022-05-27 13:23:07 +0400

    Initial commit

---
+++ Back-End Pronia/Areas/ProniaAdmin/Controllers/SliderController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Back_End_Pronia.DAL;
+using Back_End_Pronia.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Back_End_Pronia.Areas.ProniaAdmin.Controllers
+{
+    [Area("ProniaAdmin")]
+    public class SliderController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public SliderController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<Slider> slider = await _context.Sliders.ToListAsync();
+            return View(slider);
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+    }
+}
